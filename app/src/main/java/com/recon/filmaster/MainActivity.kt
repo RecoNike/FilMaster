b9bf69628b726795d67edbd1eb51b1ffb932b262
reconike <reconike@gmail.com>
2024-02-27 15:53:51 +0200

    work with fragments added

--- app/src/main/java/com/recon/filmaster/MainActivity.kt
+++ app/src/main/java/com/recon/filmaster/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.recon.filmaster
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var fragmentManager: FragmentManager
+    lateinit var fragmentTransaction: FragmentTransaction
+    lateinit var fragment: Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val searchButton: ImageButton = findViewById(R.id.searchButton)
+
+        fragmentManager = supportFragmentManager
+        fragmentTransaction = fragmentManager.beginTransaction()
+
+        // Замените PlaceholderFragment на фрагмент, который вы хотите загрузить
+        fragment = TopMoviesFragment()
+
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+
+        searchButton.setOnClickListener{
+
+            val newFragmentTransaction = fragmentManager.beginTransaction()
+
+            fragment = SearchFragment()
+            newFragmentTransaction.replace(R.id.fragmentContainer, SearchFragment())
+            newFragmentTransaction.addToBackStack(null)
+            newFragmentTransaction.commit()
+        }
+
+
     }
 }

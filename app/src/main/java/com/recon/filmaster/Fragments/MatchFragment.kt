5599f93de57e41bc35c9f0b3bd0534147ae96636
reconike <reconike@gmail.com>
2024-03-05 14:14:44 +0200

    swipe func

--- app/src/main/java/com/recon/filmaster/Fragments/MatchFragment.kt
+++ app/src/main/java/com/recon/filmaster/Fragments/MatchFragment.kt
@@ -1,15 +1,19 @@
 package com.recon.filmaster.Fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.TextView
+import android.widget.Toast
 import com.recon.filmaster.R
+import com.recon.filmaster.Utils.OnSwipeTouchListener
 
 
 class MatchFragment : Fragment() {
 
+    private lateinit var card: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,34 @@ class MatchFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_match, container, false)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        card = view.findViewById(R.id.card)
+
+        card.setOnTouchListener(object : OnSwipeTouchListener(this.requireContext()) {
+            @SuppressLint("ClickableViewAccessibility")
+            override fun onSwipeLeft() {
+                super.onSwipeLeft()
+                Toast.makeText(
+                    requireContext(), "Swipe Left gesture detected",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+
+            override fun onSwipeRight() {
+                super.onSwipeRight()
+                Toast.makeText(
+                    requireContext(),
+                    "Swipe Right gesture detected",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+
     }
 
 }

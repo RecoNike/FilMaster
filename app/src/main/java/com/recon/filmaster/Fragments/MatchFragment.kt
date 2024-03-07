baf437dda36955ee9a8765ef800139deb418ad13
reconike <reconike@gmail.com>
2024-03-07 12:40:51 +0200

    minor

--- app/src/main/java/com/recon/filmaster/Fragments/MatchFragment.kt
+++ app/src/main/java/com/recon/filmaster/Fragments/MatchFragment.kt
@@ -5,15 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.recon.filmaster.R
 import com.recon.filmaster.Utils.OnSwipeTouchListener
 
 
 class MatchFragment : Fragment() {
 
-    private lateinit var card: TextView
+    private lateinit var card: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
